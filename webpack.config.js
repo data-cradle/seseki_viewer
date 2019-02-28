8178cd182218af6ce3ff9505c35eeaaa66bdb9e2
okamura <okamura@MacBook-Air.local>
2019-02-28 14:21:50 +0900

    gh-pagesと同期

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,28 @@ var webpack = require("webpack");
 
 module.exports = {
   entry: {
-    seseki : "./src/seseki.js",
-    common : "./src/common.js"    
+    seseki: "./src/seseki.js",
+    common: "./src/common.js"
   },
   output: {
-      path: __dirname + "/build",
-      filename: "[name].bundle.js"
+    path: __dirname + "/build",
+    filename: "[name].bundle.js"
   },
   module: {
-      loaders: [
-        {
-          test: /\.css$/,
-          loader: "style!css?minimize"
-        },
-        {
-          test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-          loader: "url-loader?limit=10000&mimetype=application/font-woff"
-        },
-        {
-          test: /\.(ttf|eot|svg|png)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-          loader: "file-loader"
-        }
-      ]
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: "style!css?minimize"
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg|png)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
+      }
+    ]
   },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin()
-  ]
+  plugins: [new webpack.optimize.UglifyJsPlugin()]
 };
